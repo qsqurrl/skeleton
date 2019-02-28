b6476ee07c8286c9a7c7cac8f60e4548e95710c6
David Almaroad <david.almaroad@gmail.com>
2019-02-28 17:02:24 -0600

    Added to GIT

---
+++ src/Entity/Page.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PageRepository")
+ */
+class Page
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $route;
+
+    /**
+     * @ORM\Column(type="string", length=24)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $page_title;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRoute(): ?string
+    {
+        return $this->route;
+    }
+
+    public function setRoute(string $route): self
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPageTitle(): ?string
+    {
+        return $this->page_title;
+    }
+
+    public function setPageTitle(string $page_title): self
+    {
+        $this->page_title = $page_title;
+
+        return $this;
+    }
+}
